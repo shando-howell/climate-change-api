17e5fd8f6f8940134e756121ebe72bef6931a743
shando-howell <93793381+shando-howell@users.noreply.github.com>
2022-12-21 14:22:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// Initializing express
+const app = express();
+
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk/'
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.com/news/science-environment-56837908',
+        base: ''
+    },
+    {
+        name: 'cityam',
+        address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
+        base: ''
+
+    }
+]
+
+const articles = []
+
+// Getting newspapers helper function
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response => {
+        const html = response.data 
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")', html).each(function(){
+            const title = $(this).text()
+            const url = $(this).attr('href');
+    
+            articles.push({
+                title,
+                url: newspaper.base + url,
+                source: newspaper.name
+            })
+        })
+    })   
+})
+
+app.get('/', (req, res) => {
+    res.json('Welcome to the CC API');
+});
+
+app.get('/news', (req, res) => {
+    res.json(articles);
+})
+
+app.get('/news/:newspaperId', async(req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles);
+        }).catch(err => console.log(err))
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
